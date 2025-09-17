4adfaa630827da97087d6c299295529323983139
DodonutTheWild <paul.maillard2000@gmail.com>
2025-09-17 19:05:30 +0200

    Changement BoxContainer vers GridContainer pour avoir une grille carrée, renommage de button en tile

--- src/grid.gd
+++ src/grid.gd
@@ -1,19 +1,18 @@
-extends BoxContainer
+extends GridContainer
 
 var matrice : Array
-var button_path = preload("res://src/button/button.tscn")
-
+var tile_path : PackedScene = preload("res://src/tile/tile.tscn")
 enum UPPER_LAYER {FLAG, BOMB, QUESTION_MARK, NONE}
 
 func _ready() -> void:
 	# appeler randomize garantit un résultat pseudo aléatoire
 	randomize()
 	
-	for x in range(9):
-		for y in range(9):
-			var button = button_path.instantiate()
-			add_child(button)
-			matrice.append(button)
+	for x in range(columns):
+		for y in range(columns):
+			var tile : Button = tile_path.instantiate()
+			add_child(tile)
+			matrice.append(tile)
 	print(matrice)
 	
 	matrice.shuffle()
