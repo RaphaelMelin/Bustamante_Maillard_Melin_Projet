f572b6b953c6ed9941a0cfb40bfbb2f6db15bdbb
RaphaelMelin <raphaelmelin@orange.fr>
2025-09-17 19:38:18 +0200

    Ajout d'une fonction qui détermine les bombes + ajout du sprite de bombe + ajout de lignes dans la matrice pour retrouver plus facilement les cases + ajout de la fonction qui détermine les cases voisines d'une case donnée

--- src/grid.gd
+++ src/grid.gd
@@ -1,18 +1,35 @@
 extends GridContainer
 
-var matrice : Array
+var matrice : Array = []
 var tile_path : PackedScene = preload("res://src/tile/tile.tscn")
-enum UPPER_LAYER {FLAG, BOMB, QUESTION_MARK, NONE}
+var bomb_counter : int
+var neighbors_directions : Array[Vector2i ] = [
+	Vector2i(1, 0), Vector2i(-1, 0), Vector2i(0, 1), Vector2i(0, -1),
+	Vector2i(1, 1), Vector2i(1, -1), Vector2i(-1, 1), Vector2i(-1, -1)
+]
+enum TYPE {FLAG, BOMB, QUESTION_MARK, NONE}
 
 func _ready() -> void:
 	# appeler randomize garantit un résultat pseudo aléatoire
 	randomize()
-	
+	bomb_counter=0
 	for x in range(columns):
+		var line : Array = []
 		for y in range(columns):
 			var tile : Button = tile_path.instantiate()
+			if bomb_counter<10:
+				tile.set_type(TYPE.BOMB)
+				bomb_counter=bomb_counter+1
 			add_child(tile)
-			matrice.append(tile)
+			line.append(tile)
+		matrice.append(line)
+	matrice.shuffle()
 	print(matrice)
 	
-	matrice.shuffle()
+func get_neighbor_tiles(cell : Vector2i) -> Array:
+	# Renvoie la liste de tout les voisins
+	var neighbors: Array = []
+	for dir : Vector2i in neighbors_directions:
+		var neighbor : Vector2i = cell + dir
+		neighbors.append(neighbor)
+	return neighbors
--- src/tile/tile.gd
+++ src/tile/tile.gd
@@ -3,9 +3,19 @@ extends Button
 	
 signal left_click
 signal right_click
+var type : TYPE
+enum TYPE {FLAG, BOMB, QUESTION_MARK, NONE}
 
 func _ready():
 	connect("gui_input", _on_Button_gui_input)
+	if type == TYPE.BOMB:
+		icon=preload("res://assets/sprites/bomb.png")
+
+func set_type(type_):
+	type = type_
+
+func get_type():
+	return type
 
 func _on_Button_gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
