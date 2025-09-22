1175dd85215a70626d523360fd3a92b7fd981747
DodonutTheWild <paul.maillard2000@gmail.com>
2025-09-22 19:24:18 +0200

    Ajout des nombres autour des bombes et refactorisation

--- src/tile/tile.gd
+++ src/tile/tile.gd
@@ -6,19 +6,16 @@ signal right_click
 var type : TYPE
 enum TYPE {FLAG, BOMB, QUESTION_MARK, NONE}
 
+var value : int = 0
+var nearby_bombs_count = 0
+var grid_coords : Vector2i
+
+
+
+# Méthodes privées
+
 func _ready():
 	connect("gui_input", _on_Button_gui_input)
-	if type == TYPE.BOMB:
-		icon=preload("res://assets/sprites/bomb.png")
-
-func set_type(type_):
-	type = type_
-	match type_:
-		TYPE.BOMB:
-			icon=preload("res://assets/sprites/bomb.png")
-
-func get_type():
-	return type
 
 func _on_Button_gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
@@ -29,5 +26,55 @@ func _on_Button_gui_input(event):
 			MOUSE_BUTTON_RIGHT:
 				print("clic droit")
 				emit_signal("right_click")
-				
-				
+
+
+# Méthodes publiques
+
+func get_type() -> TYPE:
+	return type
+
+func set_type(type_):
+	type = type_
+	refresh_icon()
+	
+func get_value() -> int:
+	return value	
+
+func set_value(value_ : int) -> void:
+	value = value_	
+	
+	
+func refresh_icon() -> void:
+	#match type_:
+	#	TYPE.BOMB:
+	#		icon_name = "bomb"
+	var icon_name : String
+	match value:
+		-1: icon_name = "bomb"
+		0: icon_name = "hidden_tile"
+		1: icon_name = "tile_1"
+		2: icon_name = "tile_2"
+		3: icon_name = "tile_3"
+		4: icon_name = "tile_4"
+		5: icon_name = "tile_5"
+		6: icon_name = "tile_6"
+		7: icon_name = "tile_7"
+		8: icon_name = "tile_8"
+			
+	icon = load("res://assets/sprites/" + icon_name + ".png")
+
+
+func get_grid_coords() -> Vector2i:
+	return grid_coords
+
+func set_grid_coords(grid_coords_ : Vector2i) -> void:
+	grid_coords = grid_coords_
+
+
+func increment_nearby_bombs_count() -> void:
+	nearby_bombs_count += 1
+	
+func increment_value() -> void:
+	# Si la case est une bombre, on ne fait rien
+	if value == -1: return
+	value += 1
