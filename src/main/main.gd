958da17ce0d1b0146e974b9a3c4bae69dd419c18
DodonutTheWild <paul.maillard2000@gmail.com>
2025-10-13 18:31:51 +0200

    Modification de la taille de la fenêtre et de l'apparence graphique, appuer sur f11 pour mettre en plein écran

--- src/main/main.gd
+++ src/main/main.gd
@@ -1,4 +1,15 @@
 extends Control
 
+var fullscreen : bool = false
+
 func _ready() -> void:
 	print("Hello world")
+
+func _input(_event : InputEvent) -> void:
+	# Pour mettre en plein écran
+	if Input.is_action_just_pressed("f11"):
+		if fullscreen:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		fullscreen = !fullscreen
