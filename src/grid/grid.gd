19bc68b0c6e406883de372f4e9bce6aaf5b52062
RaphaelMelin <163831376+RaphaelMelin@users.noreply.github.com>
2025-10-31 11:10:18 +0100

    Update grid.gd

--- src/grid/grid.gd
+++ src/grid/grid.gd
@@ -61,139 +61,139 @@ func _on_reset_btn_pressed() -> void:
 
 # Génère une nouvelle matrice, peut être appelé pour écraser l'ancienne matrice lorsqu'on recommence une partie 
 func _generate_matrice() -> void:
-	# Vider l'ancienne grille
-	for child in get_children():
+	# Libérer les enfants (les tiles)
+	for child : Tile in get_children():
 		child.queue_free()
-	
+		
 	# Réinitialiser les valeurs et la matrice
 	game_started = false
 	matrice.clear()
-	_set_bomb_count(total_bombs)
+	_set_bomb_count(0)
 	veiled_tile_cpt = columns * columns
 	game_ended_lbl.text = ""
 	timer_lbl.text = "00:00"
-	
-	# Initialiser la matrice et les tiles dans la matrice
-	var unasigned_tiles: Array[Tile] = []
-	for x in range(columns):
-		var line: Array[Tile] = []
-		for y in range(columns):
-			var tile: Tile = instantiate_tile(x, y)
+
+	# Initialiser la matrice
+	var unasigned_tiles : Array = []
+	for x : int in range(columns):
+		var line : Array = []
+		for y : int in range(columns):
+			var tile : Tile = instantiate_tile(x, y)
 			unasigned_tiles.append(tile)
 			line.append(tile)
+			
 		# Ajouter la ligne de cases à la matrice
 		matrice.append(line)
-	
-	# On mélange comme ça on est surs d'avoir un placement de bombes complètement aléatoire
+		
+	# Mélanger les cases pour garantir un résultat pseudo aléatoire
 	unasigned_tiles.shuffle()
 	
 	# Assigner les bombes à la matrice
-	for tile in unasigned_tiles:
+	for tile : Tile in unasigned_tiles:
 		if bomb_count < total_bombs:
 			# Assigner le type BOMB à la case
 			tile.set_value(-1)
 			_set_bomb_count(bomb_count + 1)
 			
-			# Incrémenter les voisins
-			var coords = tile.get_grid_coords()
-			for neighbor in get_neighbor_tiles(coords):
-				neighbor.increment_value()
+			var grid_coords = tile.get_grid_coords()
+			for neighbor_tile : Tile in get_neighbor_tiles(grid_coords):
+				neighbor_tile.increment_nearby_bombs_count()
+				neighbor_tile.increment_value()	
 		else:
-			# Case sûre → affiche l'icône cachée
 			tile.refresh_front_icon()
 			break
-
-
+	
+	
 # --- Création de tiles --------------------------------------------------------------------------------
 
 # Instantie une tile (case) et l'ajoute à la scène
-func instantiate_tile(x: int, y: int) -> Tile:
+func instantiate_tile(x : int, y : int) -> Tile:
 	# Nouvelle instance de tile
-	var tile: Tile = tile_path.instantiate() as Tile
+	var tile : Button = tile_path.instantiate()
 	
 	# Connecter la tile à différent signaux
-	tile.connect("unveil_tiles_recursive", Callable(self, "unveil_tiles_recursive"))
-	tile.connect("left_click_on_bomb", Callable(self, "left_click_on_bomb"))
-	tile.connect("right_click_on", Callable(self, "right_click_on"))
-	tile.connect("right_click_off", Callable(self, "right_click_off"))
-	tile.connect("game_started", Callable(self, "on_game_started"))
-	tile.connect("tile_clicked", Callable(self, "tile_clicked"))
+	tile.connect("unveil_tiles_recursive", self.unveil_tiles_recursive)
+	tile.connect("left_click_on_bomb", self.left_click_on_bomb)
+	tile.connect("right_click_on", self.right_click_on)
+	tile.connect("right_click_off", self.right_click_off)
+	tile.connect("game_started", self.on_game_started)
+	tile.connect("tile_clicked", self.tile_clicked)
 	
 	# On définit les coordonnées de la tile
 	tile.set_grid_coords(Vector2i(x, y))
 	
 	# Ajouter la case à la scène
 	add_child(tile)
-	return tile
-
-
+	return tile	
+		
 # --- Fonctions utilitaires --------------------------------------------------------------------------------
 
-# Met à jour le compteur et le label
-func _set_bomb_count(value: int) -> void:
+func _set_bomb_count(value) -> void:
 	bomb_count = value
-	bomb_count_lbl.text = str(bomb_count)
+	bomb_count_lbl.text = str(bomb_count)	
 
-# Retourne les 8 voisins valides (ou moins sur les bords)
-func get_neighbor_tiles(grid_coords: Vector2i) -> Array[Tile]:
-	var neighbors: Array[Tile] = []
-	for dir in neighbors_directions:
-		var neighbor_coords = grid_coords + dir
-		var tile = get_tile(neighbor_coords)
-		if tile != null:
-			neighbors.append(tile)
-	return neighbors
+	
+func get_neighbor_tiles(grid_coords : Vector2i) -> Array:
+	# Renvoie la liste de tout les voisins d'une tile
+	var neighbor_tiles: Array = []
+	for direction : Vector2i in neighbors_directions:
+		var neighbor_grid_coords : Vector2i = grid_coords + direction
+		var neighbor_tile = get_tile(neighbor_grid_coords)
+		
+		if neighbor_tile != null:
+			neighbor_tiles.append(neighbor_tile)
+	return neighbor_tiles
 
-# Récupère une tile par coordonnées (ou null si hors grille)
+
 func get_tile(grid_coords: Vector2i) -> Tile:
 	# On vérifie que la tile n'est pas en dehors de la matrice
-	if (grid_coords.x < 0 or grid_coords.x >= matrice.size() or
-		grid_coords.y < 0 or grid_coords.y >= matrice.size()):
+	if grid_coords.x < 0 or grid_coords.x >= matrice.size() \
+		or grid_coords.y < 0 or grid_coords.y >= matrice.size():
 		return null
 	return matrice[grid_coords.x][grid_coords.y]
-
+	
 
 # --- Mécaniques --------------------------------------------------------------------------------
 
 # Méthode appelée lorsque l'on révèle une case vide
 # Révèle les cases vides et leurs voisins de manière récursive
-func unveil_tiles_recursive(tile: Tile) -> void:
+func unveil_tiles_recursive(tile : Tile) -> void:
+	var neighbor_tiles : Array = get_neighbor_tiles(tile.get_grid_coords())
 	veiled_tile_cpt -= 1
 	tile.set_type(Tile.TYPE.UNVEILED)
 	tile.refresh_icon()
-	
-	# Arrête l'appel si la case est un chiffre
 	if tile.value != 0:
 		return
 	
 	# Si la case est vide, on propage le dévoilage des tiles aux autres voisins
-	for neighbor in get_neighbor_tiles(tile.get_grid_coords()):
+	for neighbor:Tile in neighbor_tiles:
 		if neighbor.type != Tile.TYPE.UNVEILED:
 			unveil_tiles_recursive(neighbor)
+	
 
 # Méthode appelée lorsqu'on clique sur une bombe
 # Révèle les bombes qui ne sont pas marquées par un drapeau et met fin à la partie
 func left_click_on_bomb() -> void:
 	game_ended_lbl.text = "Défaite ):"
-	for x in range(columns):
-		for y in range(columns):
-			var tile = get_tile(Vector2i(x, y))
-			# Révéler les bombes non marquées
+	for x : int in range(columns):
+		for y : int in range(columns):
+			var coords_tile : Vector2i = Vector2i(x, y)
+			var tile : Tile =  get_tile(coords_tile)
 			if tile.value == -1 and tile.type != Tile.TYPE.FLAG:
 				tile.set_type(Tile.TYPE.UNVEILED)
 				tile.refresh_icon()
-	on_game_ended()
-
+				on_game_ended()
+	
 # Méthode appelée lors d'un clic droit sur une tile	non retournée
 # Augmente le compteur de bombes de 1
 func right_click_on() -> void:
 	_set_bomb_count(bomb_count - 1)
-
+	
 # Méthode appelée lors d'un clic droit sur une tile	non retournée et avec un drapeau
 # Réduit le compteur de bombes de 1
 func right_click_off() -> void:
 	_set_bomb_count(bomb_count + 1)
-
+	
 # Méthode appelé à chaque clic sur une tile
 # Mets fin à la partie si toutes les cases ont été retourné sauf les bombes
 func tile_clicked() -> void:
@@ -201,19 +201,19 @@ func tile_clicked() -> void:
 	if veiled_tile_cpt == total_bombs:
 		on_game_ended()
 		game_ended_lbl.text = "Victoire :)"
+		
 
 # Méthode appelée lorsque l'on clique sur une tile pour la première fois de la partie
 # Démarre le timer
 func on_game_started() -> void:
-	if game_started:
-		return
+	if game_started: return
 	game_started = true
 	timer_lbl.set_is_game_ended(false)
-
+	
 # Méthode appelée lorsque l'on perds ou gagne la partie
 # Mets fin au timer et empêche de continuer à jouer
 func on_game_ended() -> void:
-	timer_labl.set_is_game_ended(true)
-	for tile in get_children():
+	timer_lbl.set_is_game_ended(true)
+	for tile : Tile in get_children():
 		tile.disable_clics = true
-		tile.disabled = true  # pour l'apparence
+		tile.disabled = true # pour l'apparence
