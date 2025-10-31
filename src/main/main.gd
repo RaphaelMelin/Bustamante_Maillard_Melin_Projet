05bb393496554ec7d3e03ff6f1e2573cfb5017ff
RaphaelMelin <163831376+RaphaelMelin@users.noreply.github.com>
2025-10-31 11:10:01 +0100

    Update main.gd

--- src/main/main.gd
+++ src/main/main.gd
@@ -11,11 +11,11 @@ var fullscreen: bool = false
 
 
 # Gère les entrées clavier
-func _input(event: InputEvent) -> void:
+func _input(_event : InputEvent) -> void:
 	# Pour mettre en plein écran
 	if Input.is_action_just_pressed("f11"):
+		if fullscreen:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		fullscreen = !fullscreen
-		var mode = (DisplayServer.WINDOW_MODE_FULLSCREEN
-					if fullscreen
-					else DisplayServer.WINDOW_MODE_WINDOWED)
-		DisplayServer.window_set_mode(mode)
